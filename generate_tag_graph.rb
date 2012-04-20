b6cf507d4d0225fd067a84db1ac5aa431c9ecc5a
Senthil <senthilv@thoughtworks.com>
2012-04-20 19:50:30 +0530

    Sen | RGL graph and data structure

---
+++ generate_tag_graph.rb
@@ -0,0 +1,5 @@
+url_tags = [
+  {:url => "http://guides.rubyonrails.org/3_2_release_notes.html", :tags => ['technology', 'rails', 'rails 3.2', 'ruby']},
+  {:url => "http://www.rubyinside.com/ruby-1-9-3-preview-1-released-5229.html", :tags => ['technology', 'ruby 1.9.3', 'ruby']},
+  {:url => "http://www.bbc.co.uk/news/world-africa-17787142", :tags => ['news', 'bbc', 'sudan']}
+]
