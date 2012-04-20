d2078afe3f83b3548de0c1f371c7544e79ca581c
Senthil <senthilv@thoughtworks.com>
2012-04-20 20:23:52 +0530

    Sen | Creates a mind map with tags.

--- generate_tag_graph.rb
+++ generate_tag_graph.rb
@@ -3,3 +3,8 @@ url_tags = [
   {:url => "http://www.rubyinside.com/ruby-1-9-3-preview-1-released-5229.html", :tags => ['technology', 'ruby 1.9.3', 'ruby']},
   {:url => "http://www.bbc.co.uk/news/world-africa-17787142", :tags => ['news', 'bbc', 'sudan']}
 ]
+
+tag_mapping = []
+url_tags.collect{|url_tag| ["My links", *url_tag[:tags]].each_cons(2){|a, b| tag_mapping << a; tag_mapping << b}}.flatten
+dg=RGL::DirectedAdjacencyGraph[*tag_mapping]
+dg.write_to_graphic_file('jpg', "tag_map")
