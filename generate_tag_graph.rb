2ba4634ae65e1cc4f9fe370e5bd9cfcf7625975f
Senthil <senthilv@thoughtworks.com>
2012-04-20 22:13:46 +0530

    Sen | Links also added to graph.

--- generate_tag_graph.rb
+++ generate_tag_graph.rb
@@ -1,10 +1,18 @@
+require 'rgl/adjacency'
+require 'rgl/dot'
+
 url_tags = [
-  {:url => "http://guides.rubyonrails.org/3_2_release_notes.html", :tags => ['technology', 'rails', 'rails 3.2', 'ruby']},
-  {:url => "http://www.rubyinside.com/ruby-1-9-3-preview-1-released-5229.html", :tags => ['technology', 'ruby 1.9.3', 'ruby']},
+  {:url => "http://guides.rubyonrails.org/3_2_release_notes.html", :tags => ['technology', 'rails', 'rails 3.2']},
+  {:url => "http://www.rubyinside.com/ruby-1-9-3-preview-1-released-5229.html", :tags => ['technology', 'rails 3.2', 'ruby 1.9.3', 'ruby']},
   {:url => "http://www.bbc.co.uk/news/world-africa-17787142", :tags => ['news', 'bbc', 'sudan']}
 ]
 
+ROOT_TAG_NAME = "My links"
+
 tag_mapping = []
-url_tags.collect{|url_tag| ["My links", *url_tag[:tags]].each_cons(2){|a, b| tag_mapping << a; tag_mapping << b}}.flatten
+url_tags.collect{|url_tag| [ROOT_TAG_NAME, *url_tag[:tags]].each_cons(2){|a, b| tag_mapping << a; tag_mapping << b}}.flatten
+distinct_tags = dg.vertices - [ROOT_TAG_NAME]
+leaf_tags_mapping = url_tags.inject({}){|r, url_tag| r.merge(url_tag[:url] => url_tag[:tags].last)}
+leaf_tags_mapping.collect {|url, leaf_tag|  tag_mapping << leaf_tag; tag_mapping << url}
 dg=RGL::DirectedAdjacencyGraph[*tag_mapping]
 dg.write_to_graphic_file('jpg', "tag_map")
